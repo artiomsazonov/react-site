69483beb54b6057890062af85e2a89403667f46c
artiomsazonov <55507012+artiomsazonov@users.noreply.github.com>
2021-01-29 17:08:50 +0300

    rooter

--- src/Ccomponent.js
+++ src/Ccomponent.js
@@ -1,55 +1,44 @@
 import React, { Component } from 'react'
 import './stylesNew.css';
-//import Fcomponent from "./Fcomponent";
-//import Hcomponent from "./Hcomponent";
+import Home from "./Home";
+import About from "./About";
+import Users from "./Users";
+import { Link, Switch } from 'react-router-dom';
 
 
 export default class Ccomponent extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
+
     };
   }
 
-  componentDidMount() {
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result.drinks
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
 
   render() {
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <p>Error {error.message}</p>
-    } else if (!isLoaded) {
-      return <p>Loading...</p>
-    } else {
-      return (
-        <ul>
-          {items.map(item => (
-            <li key={item.name}>
-              {item.strDrink}
-              <img width="50" height="50" src={item.strDrinkThumb} />
-            </li>
-          ))}
-        </ul>
-      )
-    }
+
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+
+          </Switch>
+        </div>
+      </Router>
+    )
   }
+}
 }
