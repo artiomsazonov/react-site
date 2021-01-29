e2196f410fcbfae8825637a710d8299b9e645e9f
artiomsazonov <55507012+artiomsazonov@users.noreply.github.com>
2021-01-29 16:48:35 +0300

    fetch

--- src/Ccomponent.js
+++ src/Ccomponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './stylesNew.css';
-import Fcomponent from "./Fcomponent";
+//import Fcomponent from "./Fcomponent";
 //import Hcomponent from "./Hcomponent";
 
 
@@ -8,22 +8,48 @@ export default class Ccomponent extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      name: "Button not pressed"
+      error: null,
+      isLoaded: false,
+      items: []
     };
-    this.updateData = this.updateData.bind(this)
   }
-  updateData = (value) => {
-    this.setState({
-      name: value
-    })
-  }
-  render() {
-    return (
-      <div className="container">
-        <p>State:{this.state.name}</p>
-        <Fcomponent updateData={this.updateData} />
 
-      </div>
-    )
+  componentDidMount() {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.drinks
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <p>Error {error.message}</p>
+    } else if (!isLoaded) {
+      return <p>Loading...</p>
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.name}>
+              {item.strDrink}
+              <img width="50" height="50" src={item.strDrinkThumb} />
+            </li>
+          ))}
+        </ul>
+      )
+    }
   }
 }
